481dce156c6ff1fa7554eae62f7b09c0553323d0
MinhNghia-0910 <22t1020684@husc.edu.vn>
2025-08-30 12:01:32 +0700

    Upload ECS-Lab3

---
+++ AWS-ECS-Lab3/README.md
@@ -0,0 +1,6 @@
+# NIENLUAN2025
++ Thực hành cơ bản về ECS
++ clone git
++ run mvn clean install
++ docker-compose build . 
+
---
+++ AWS-ECS-Lab3/start.sh
@@ -0,0 +1,52 @@
+!/bin/bash
+# Tạo repo trước khi push và lấy link repo
+DOCKER_REPO_URI=789922128457.dkr.ecr.us-east-1.amazonaws.com
+
+
+function build() {
+    echo "Building service"
+    docker-compose build .
+}
+
+function push() {
+    #DOWNLOAD AWS CLI VA TAO IAM (ROLE=AWSEC2CONTAINERFULLACCESS).
+    echo "Push images"
+    $(aws ecr get-login --no-include-email --region us-east-1)
+
+    docker tag auth-jwt-service:latest 789922128457.dkr.ecr.us-east-1.amazonaws.com/auth-jwt-service:latest
+    docker tag customer-service:latest 789922128457.dkr.ecr.us-east-1-1.amazonaws.com/customer-service:latest
+    docker tag gateway:latest 789922128457.dkr.ecr.us-east-1.amazonaws.com/gateway:latest
+    docker tag redis:latest 789922128457.dkr.ecr.us-east-1.amazonaws.com/redis:latest
+    docker tag sec-service:latest 789922128457.dkr.ecr.us-east-1.amazonaws.com/sec-service:latest
+    docker tag web-angular:latest 789922128457.dkr.ecr.us-east-1.amazonaws.com/web-angular:latest
+
+    docker push $DOCKER_REPO_URI/auth-jwt-service:latest
+    docker push $DOCKER_REPO_URI/customer-service:latest
+    docker push $DOCKER_REPO_URI/gateway:latest
+    docker push $DOCKER_REPO_URI/redis:latest
+    docker push $DOCKER_REPO_URI/sec-service:latest
+    docker push $DOCKER_REPO_URI/web-angular:latest
+}
+
+function options() {
+      echo "./start build -> Build docker images"
+      echo "./start push -> Push images to docker registry. Plz configure repo  before push."
+}
+
+
+
+for var in "$@"
+do
+  case "$var" in
+    build)
+      build
+      ;;
+    push)
+      push
+      ;;
+    *)
+      options
+      ;;
+  esac
+done
+
